36db82916465bb5a7d2d0f4bc8b90e048a671849
majorstirrer <magickotlovski@gmail.com>
2020-05-01 10:52:40 +0100

    adding link between pages

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 var express = require('express');
 var app = express();
-
 var request = require('request');
 
 app.set("view engine", "ejs");
 
-
+// ROUTES:
 app.get('/', function(req, res){
     res.render('landing');
 });
 
+app.get('/campgrounds', function(req, res){
+    var campgrounds = [
+        {name: "Salmon Creek", image: "https://pixabay.com/get/52e5d7414355ac14f1dc84609620367d1c3ed9e04e5074417d2d7fdd964fc7_340.jpg"},
+        {name: "Granite Hill", image: "https://pixabay.com/get/57e8d1464d53a514f1dc84609620367d1c3ed9e04e5074417d2d7fdd964fc7_340.jpg"},
+        {name: "Bieszkowice", image: "https://pixabay.com/get/57e8d0424a5bae14f1dc84609620367d1c3ed9e04e5074417d2d7fdd964fc7_340.jpg"}
+    ]
+    res.render('campgrounds');
+});
+    
 
 
 
+// PORT LISTENING:
 app.listen(3000, () => console.log('server connected'));
