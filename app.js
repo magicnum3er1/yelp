dfd32bf9b25bfaaaaf33d0337305433fad04391c
majorstirrer <magickotlovski@gmail.com>
2020-05-04 11:04:20 +0100

    starting the styling with Bootstrap3

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 
 var campgrounds = [
-    {name: "Salmon Creek", image: "https://pixabay.com/get/52e5d7414355ac14f1dc84609620367d1c3ed9e04e5074417d2d7fdd964fc7_340.jpg"},
-    {name: "Granite Hill", image: "https://pixabay.com/get/57e8d1464d53a514f1dc84609620367d1c3ed9e04e5074417d2d7fdd964fc7_340.jpg"},
-    {name: "Bieszkowice", image: "https://pixabay.com/get/57e8d0424a5bae14f1dc84609620367d1c3ed9e04e5074417d2d7fdd964fc7_340.jpg"}
+    {name: "Salmon Creek", image: "https://www.globtroter.pl/zdjecia/polska/b280888_polska_kaszuby_pojezierze_kaszubskie.jpg"},
+    {name: "Granite Hill", image: "https://polskiepomorze.pl/images/intro/pojezierze-kaszubskie.jpg"},
+    {name: "Bieszkowice", image: "https://img4.garnek.pl/a.garnek.pl/009/807/9807159_800.0.jpg/pojezierze-kaszubskie.jpg"}
 ]
 
 // ROUTES:
