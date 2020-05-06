20431ebfc9917e7e51fb8f50b297b23bac364e49
majorstirrer <magickotlovski@gmail.com>
2020-05-06 10:41:39 +0100

    adding all sort of shit, I forgot to save earlier

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
-var express = require('express');
-var app = express();
-var request = require('request');
-var bodyParser = require("body-parser");
+var express         = require('express');
+var app             = express();
+var request         = require('request');
+var bodyParser      = require("body-parser");
+var mongoose        = require('mongoose');
+
+// Mongoose:
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ mongoose.connect('mongodb://localhost/yelp_camp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-    {name: "Salmon Creek", image: "https://www.globtroter.pl/zdjecia/polska/b280888_polska_kaszuby_pojezierze_kaszubskie.jpg"},
-    {name: "Granite Hill", image: "https://polskiepomorze.pl/images/intro/pojezierze-kaszubskie.jpg"},
-    {name: "Bieszkowice", image: "https://img4.garnek.pl/a.garnek.pl/009/807/9807159_800.0.jpg/pojezierze-kaszubskie.jpg"},
-    {name: "Bieszkowice", image: "https://img4.garnek.pl/a.garnek.pl/009/807/9807159_800.0.jpg/pojezierze-kaszubskie.jpg"},
-    {name: "Bieszkowice", image: "https://img4.garnek.pl/a.garnek.pl/009/807/9807159_800.0.jpg/pojezierze-kaszubskie.jpg"}
-]
+// Schema setup:
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//     {
+//         name: "dupa",
+//         image: "https://g7.gazetaprawna.pl/p/_wspolne/pliki/3322000/3322811-jezioro-kluczysko-lezy-w-obrebie.jpg",
+//         description:"Rubbish everywhere, lot of drunk hillbillies and a smell of shit"
+//     }, function(err, campground){
+//         if(err){
+//             console.log(err);
+//         } else {
+
+//             console.log("newly created camp");
+//             console.log(campground);
+//         }
+//     });
+
 
 // ROUTES:
 app.get('/', function(req, res){
@@ -20,25 +46,39 @@ app.get('/', function(req, res){
 });
 
 app.get('/campgrounds', function(req, res){
-    
-    res.render('campgrounds', {campgrounds:campgrounds});
-
-});
-
-    
-app.post("/campgrounds",function(req, res){
-    var name = req.body.name;
-    var image = req.body.image;
-    var newCampground = {name: name, image: image}
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds")
-
-
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else{
+            res.render('index', {campgrounds:allCampgrounds});
+        }
+    })
 });
 
 app.get("/campgrounds/new", function(req, res){
     res.render("new.ejs")
+});
+
+app.get("/campgrounds/:id", function(req, res){
+    res.render("show")
 })
+
+//  POST ROUTE:
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   
+app.post("/campgrounds",function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image}
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err)
+        } else {
+            res.redirect("/campgrounds")
+        }
+    })
+   });
+
+
 
 
 
